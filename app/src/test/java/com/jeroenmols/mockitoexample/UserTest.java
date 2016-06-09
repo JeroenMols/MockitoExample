bc755d15d7fb98871aae697e50b531c63d066d4d
Jeroen Mols <jeroen.mols.be@gmail.com>
2016-06-09 17:23:24 +0200

    Merged annotation tests with non annotation ones

--- app/src/test/java/com/jeroenmols/mockitoexample/UserTest.java
+++ app/src/test/java/com/jeroenmols/mockitoexample/UserTest.java
@@ -1,6 +1,10 @@
 package com.jeroenmols.mockitoexample;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyInt;
@@ -23,16 +27,26 @@ public class UserTest {
     public static final int USER_ID = 1111007;
     public static final String PASSWORD = "n1c3try";
 
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Mock
+    private WebService mockWebService;
+
     @Test
-    public void createUserWithWebService() throws Exception {
+    public void createUserWithoutAnnotation() throws Exception {
         WebService mockWebService = mock(WebService.class);
 
         new User(mockWebService, 0, null);
     }
 
     @Test
+    public void createUser() throws Exception {
+        new User(mockWebService, 0, null);
+    }
+
+    @Test
     public void logoutOfWebservice() throws Exception {
-        WebService mockWebService = mock(WebService.class);
         User user = new User(mockWebService, USER_ID, PASSWORD);
 
         user.logout();
@@ -48,7 +62,6 @@ public class UserTest {
 
     @Test
     public void loginToWebservice() throws Exception {
-        WebService mockWebService = mock(WebService.class);
         User user = new User(mockWebService, USER_ID, PASSWORD);
 
         user.login(null);
@@ -59,7 +72,6 @@ public class UserTest {
 
     @Test
     public void loginWithUserNameAndPassword() throws Exception {
-        WebService mockWebService = mock(WebService.class);
         User user = new User(mockWebService, USER_ID, PASSWORD);
 
         user.login(null);
